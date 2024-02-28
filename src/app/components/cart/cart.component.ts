0267f51da006499a3ca1ab05b3605c5fe85c8f6e
Frederic Bec <bec.frederic90@outlook.fr>
2024-02-28 13:47:46 +0100

    Add cart component

---
+++ src/app/components/cart/cart.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Training } from 'src/app/model/training.model';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+
+  cart : Array<Training> = [];
+  total : number |undefined;
+
+  constructor(private cartService : CartService) {
+  }
+
+  ngOnInit(): void {
+    this.cart = this.cartService.getCart();
+    this.total = this.cartService.totalCart();
+
+  }
+
+  removeTraining(training : Training){
+    this.cartService.removeFromCart(training);
+  }
+
+
+
+}
