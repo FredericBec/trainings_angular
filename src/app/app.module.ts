331ca90f2e2d784b68b7956d9fc78eec58b2bc36
Frederic Bec <bec.frederic90@outlook.fr>
2024-02-28 13:46:07 +0100

    Add routes to app-routing module and formsmodule to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrainingsComponent } from './components/trainings/trainings.component';
+import { FormsModule } from '@angular/forms';
+import { CartComponent } from './components/cart/cart.component';
+import { CartService } from './services/cart.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrainingsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [CartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
