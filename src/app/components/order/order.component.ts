111a7fd1a975e504b9c84534f3962dcb7e66feb7
Frederic Bec <bec.frederic90@outlook.fr>
2024-02-29 16:14:41 +0100

    Add order component

---
+++ src/app/components/order/order.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Customer } from 'src/app/model/customer.model';
+import { Training } from 'src/app/model/training.model';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.css']
+})
+export class OrderComponent implements OnInit {
+
+  cart : Map<number, Training>;
+  total : number | undefined;
+  customer : Customer;
+  constructor(private cartService : CartService) { 
+    this.cart = new Map<number, Training>();
+    this.customer = new Customer('','','','','');
+  }
+
+  ngOnInit(): void {
+   this.cart =  this.cartService.getCart();
+   this.total = this.cartService.totalCart();
+   const customerInfo = this.cartService.getCustomer();
+    if(customerInfo){
+      this.customer = customerInfo;
+    }
+  }
+
+  
+}
